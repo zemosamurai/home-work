d528c6fd758e202bd5c35642e5398d65c6dab62b
zemosamurai <zemosamurai@gmail.com>
2022-11-16 14:26:26 +0300

    1

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -20,6 +20,7 @@ function Pages() {
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
                 <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
 
+
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
                 <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
                 <Route path={PATH.JUNIOR} element={<Junior/>}/>

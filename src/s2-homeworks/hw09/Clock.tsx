7b2b9d8f9714c12130f308dff70da121a48e5f7c
zemosamurai <zemosamurai@gmail.com>
2022-12-03 17:16:11 +0300

    add time display and hower

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -27,12 +27,22 @@ function Clock() {
 
     }
 
-    const stringTime = 'date->time' || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    let formatterMount = new Intl.DateTimeFormat("en", {
+        month: "long"
+    })
+
+    const stringTime = date.toLocaleTimeString("ru-RU") || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringDate = formatterMount.format(date) || <br/> // при наведении
+    // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+
+
+    let formatterDay = new Intl.DateTimeFormat("en", {
+        weekday: "long",
+    })
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = 'date->day' || <br/> // пишут студенты
-    const stringMonth = 'date->month' || <br/> // пишут студенты
+    const stringDay = formatterDay.format(date)|| <br/> // пишут студенты
+    const stringMonth = date.toLocaleDateString("ru-RU")|| <br/> // пишут студенты при наведении
 
     return (
         <div className={s.clock}>

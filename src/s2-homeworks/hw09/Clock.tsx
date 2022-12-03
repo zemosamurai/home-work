a49ccb98efd6d43d728167bb84b871a96e7a49b1
zemosamurai <zemosamurai@gmail.com>
2022-12-03 18:20:23 +0300

    refactor

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -16,11 +16,13 @@ function Clock() {
             setDate(new Date(restoreState('hw9-date', Date.now())))
         }, 1000)
         setTimerId(+newDimerId)
+
     }
 
     const stop = () => {
         // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
         clearInterval(timerId)
+        setTimerId(undefined)
     }
 
     const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
@@ -33,19 +35,18 @@ function Clock() {
     let formatterMount = new Intl.DateTimeFormat("en", {
         month: "long"
     })
-
-    const stringTime = date.toLocaleTimeString("ru-RU") || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = formatterMount.format(date) || <br/> // при наведении
-    // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
-
-
     let formatterDay = new Intl.DateTimeFormat("en", {
         weekday: "long",
     })
 
+    const stringTime = date.toLocaleTimeString("ru-RU") || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringDate =  date.toLocaleDateString("ru-RU") || <br/> // при наведении
+    // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+
+
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = formatterDay.format(date)|| <br/> // пишут студенты
-    const stringMonth = date.toLocaleDateString("ru-RU")|| <br/> // пишут студенты при наведении
+    const stringMonth = formatterMount.format(date) || <br/> // пишут студенты при наведении
 
     return (
         <div className={s.clock}>
@@ -79,14 +80,14 @@ function Clock() {
             <div className={s.buttonsContainer}>
                 <SuperButton
                     id={'hw9-button-start'}
-                    disabled={false} // пишут студенты // задизэйблить если таймер запущен
+                    disabled={typeof(timerId) === 'number'} // пишут студенты // задизэйблить если таймер запущен
                     onClick={start}
                 >
                     start
                 </SuperButton>
                 <SuperButton
                     id={'hw9-button-stop'}
-                    disabled={false} // пишут студенты // задизэйблить если таймер не запущен
+                    disabled={typeof(timerId) !== 'number'} // пишут студенты // задизэйблить если таймер не запущен
                     onClick={stop}
                 >
                     stop

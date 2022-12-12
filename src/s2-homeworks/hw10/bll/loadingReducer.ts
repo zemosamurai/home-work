986bf6c75b6f00fbbcf378b0405327e8c69f5cdf
zemosamurai <zemosamurai@gmail.com>
2022-12-12 17:46:10 +0300

    add logic from reducer

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,14 +2,20 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+type InitialState = typeof initState
+
+export const loadingReducer = (state = initState, action: LoadingActionType): InitialState => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING': {
+            return {...state, isLoading: action.isLoading}
+        }
         default:
             return state
     }
 }
+
+
 
 type LoadingActionType = {
     type: 'CHANGE_LOADING'

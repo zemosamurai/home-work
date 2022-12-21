0a62c4341ec2d4b266858cbfb900bd828d7ea47b
zemosamurai <zemosamurai@gmail.com>
2022-12-21 03:50:31 +0300

    uncommit HW11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -15,8 +15,16 @@ function HW11() {
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    const change = (event: any, value: any) => {
+    const change = (event: any, value: number | number[]) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+        if(Array.isArray(event.target.value)) {
+            const val = event.target.value
+            setValue1(val[0])
+            setValue2(val[1])
+        } else {
+            setValue1(event.target.value)
+        }
+
     }
 
     return (
@@ -30,7 +38,9 @@ function HW11() {
                         <SuperRange
                             id={'hw11-single-slider'}
                             // сделать так чтоб value1 изменялось // пишет студент
-
+                            value={value1}
+                            name={'hw11-single-slider'}
+                            onChange={change}
                         />
                     </div>
                     <div className={s.wrapper}>
@@ -38,7 +48,8 @@ function HW11() {
                         <SuperRange
                             id={'hw11-double-slider'}
                             // сделать так чтоб value1/2 изменялось // пишет студент
-
+                            value={[value1, value2]}
+                            onChange={change}
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
